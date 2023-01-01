303878094fa1c8974071ecdae71a4528b4e6dacf
Marco <marco14lyoko@gmail.com>
2023-01-01 17:53:23 +0000

    Commented main.pl and utils.pl

--- src/main.pl
+++ src/main.pl
@@ -1,3 +1,4 @@
+% Includes all the modules
 :- use_module(library(lists)).
 :- use_module(library(apply)).
 :- use_module(library(between)).
@@ -5,28 +6,35 @@
 :- use_module(library(random)).
 :- use_module(library(system)).
 
+% Includes all the files
 :- ensure_loaded('utils.pl').
 :- ensure_loaded('io.pl').
 :- ensure_loaded('logic.pl').
 :- ensure_loaded('logic_utils.pl').
 :- ensure_loaded('logic_ai.pl').
-% MAIN
+
+% player(+Player)/1
+% Player can be either human or computer
 player(human).
 player(computer).
 
+% phase(+Phase)/1
+% Phase can be either capture, drop or peek
 phase(drop).
 phase(capture).
 phase(peek).
 
+% play/0
+% Main function that starts the game
 play:-
 	repeat,
-	game_menu_show,
-	read_until_between(0,3, OPT),
+	game_menu_show, % Shows the game menu
+	read_until_between(0,3, OPT), % Reads the input option
 	switch(OPT, [
-		0: (!),
-		1: set_game_state(human-human-0),
-		2: display_level_pc(human-computer),
-		3: display_level_pc(computer-computer)
+		0: (!), % Exits the game
+		1: set_game_state(human-human-0), % Sets the game state to human vs human
+		2: display_level_pc(human-computer), % Sets the game state to human vs computer
+		3: display_level_pc(computer-computer) % Sets the game state to computer vs computer
 	]),
-	OPT = 0 -> !;
-	nl, write('End of game.'), nl, nl, fail.
+	OPT = 0 -> !; % If 0 is selected, exit the game
+	nl, write('End of game.'), nl, nl, fail. % Restart the game loop
--- src/utils.pl
+++ src/utils.pl
@@ -1,28 +1,28 @@
-% switch(+Variable, +[Value:Goal|Cases])
+% switch(+Variable, +[Value:Goal|Cases])/2
 % Variable to be tested against Value, if it matches, Goal is called, otherwise the next case is tested.
 switch(Variable, [Value:Goal|Cases]):-
 	(Variable = Value -> % Test if the variable matches the value
 	call(Goal); % If the variable matches the value, call the goal
 	switch(Variable, Cases)). % Otherwise, test the next case
 
-% succ(+Current, -Next)
+% succ(+Current, -Next)/2
 % Next is the successor of Current in a sequence.
 succ(Current, Next):- Next is Current+1. % Next is Current+1.
 
-% abs(+Value, -AbsoluteValue)
+% abs(+Value, -AbsoluteValue)/2
 % AbsoluteValue is the absolute value of Value.
 abs(Value, AbsoluteValue):- Value >= 0, AbsoluteValue is Value. % Positive value case
 abs(Value, AbsoluteValue):- Value < 0, AbsoluteValue is -Value. % Negative value case
 
 % ======================= LIST UTILS =======================
 
-% isList(+List)
+% isList(+List)/1
 % True if list is given.
 isList([_|_]). % Non-empty list is a list.
 isList([]). % Empty list is also a list.
 
 
-%countElem_(+List, +Elem, +Acc, -Count)
+%countElem_(+List, +Elem, +Acc, -Count)/4
 % Count is the number of times Elem appears in List.
 countElem_([], _, Acc, Acc):-!. % Base case
 countElem_([H|T], E, Acc, Ret):- % Recursive case
@@ -31,11 +31,11 @@ countElem_([H|T], E, Acc, Ret):- % Recursive case
 		countElem_(T, E, Acc1, Ret); 
 	countElem_(T, E, Acc, Ret).
 	
-% countElem(+List, +Elem, -Count)
+% countElem(+List, +Elem, -Count)/3
 % Count is the number of times Elem appears in List.	
 countElem(List, Elem, Count):-countElem_(List, Elem, 0, Count).
 
-% flatten(+List, -Flat)
+% flatten(+List, -Flat)/2
 % Turns List of lists into a Flat list.
 flatten([], []). % Base case
 flatten([H|T], Flat) :-
@@ -46,7 +46,7 @@ flatten([H|T], Flat) :-
 flatten([H|T], [H|Flat]) :-
 	flatten(T, Flat).
 
-% insert_elem(+Index, +List, +Elem, -NewList)
+% insert_elem(+Index, +List, +Elem, -NewList)/4
 % Inserts Elem at Index in List and returns the NewList.
 insert_elem(0, [H|T], E, NewList):- append([E,H], T, NewList), !. % If the index is 0, just add the element
 insert_elem(_, [], E, NewList):- NewList = [E],!. % If the list is empty, just add the element
@@ -56,7 +56,7 @@ insert_elem(I, [H|T], E, NewList):- % Otherwise, recursively add the element
 	insert_elem(I1, T, E, L),
 	append([H], L, NewList).
   
-% delete_elem(+Index, +List, -NewList)
+% delete_elem(+Index, +List, -NewList)/3
 % Deletes the element at Index in List and returns the NewList.  
 delete_elem(0, [_|T], NewList):- append([], T, NewList), !. % If the index is 0, just remove the element
 delete_elem(I, [H|T], NewList):- % Otherwise, recursively remove the element
@@ -65,7 +65,7 @@ delete_elem(I, [H|T], NewList):- % Otherwise, recursively remove the element
 	delete_elem(I1, T, L),
 	append([H], L, NewList).
 
-% list_replace(+List, +Index, +Value, -New_List)
+% list_replace(+List, +Index, +Value, -New_List)/4
 % Replaces the element at Index in List with Value and returns the New_List.
 list_replace(List, Index, Value, New_List):-
 	delete_elem(Index, List, List_Aux), % Delete the element at Index
@@ -73,7 +73,7 @@ list_replace(List, Index, Value, New_List):-
 
 % ====================== RUN LENGTH ENCODING ======================
 
-% rle(+List, -Encoded)
+% rle(+List, -Encoded)/2
 % Encoded is the run length encoding of List.
 % Run length encoding is a list of lists, where each sublist is of the form [Count, Value].
 rle([], []):-!. % Base case
@@ -87,12 +87,12 @@ rle([X|XT], [[1, X], [SubCount, Y] | RestEncoded]) :-
 
 % ====================== Matrix Generation ======================
 
-% gen_2d_array(+N, +M, +C, -Matrix)
+% gen_2d_array(+N, +M, +C, -Matrix)/4
 % Generates a N x M matrix with all elements equal to C.
 gen_2d_array(0, _, C, []):-!.
 gen_2d_array(N, M, C, [H|T]):- N > 0, gen_1d_array(M, C, H), N1 is N-1, gen_2d_array(N1, M, C, T).
 
-% gen_1d_array(+N, +C, -Array)
+% gen_1d_array(+N, +C, -Array)/3
 % Generates a 1D array of size N with all elements equal to C.
 gen_1d_array(0, C, []):-!.
 gen_1d_array(N, C, [H|T]):- N > 0, H = C, N1 is N-1, gen_1d_array(N1, C, T).
